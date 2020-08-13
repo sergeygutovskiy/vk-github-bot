dd5e7ee30dd5f0e7cceca60c282a83f976f2b243
sergeygutovskiy <sergey.gutovsk@gmail.com>
2020-08-14 02:24:07 +0300

    test

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/api/update", (req, res) => {
 		message += "\n";
 	}
 
-	console.log(message);
+	// console.log(message);
 
 	(async () => {
 
@@ -83,24 +83,27 @@ app.post("/api/update", (req, res) => {
 
 		// console.log(server + " " + key + " " + ts + '\n');
 
+		// let message = "я русский!!!"
+
 		let url = "https://api.vk.com/method/"
 			+ "messages.send"
 			+ "?random_id=" + Math.floor((1000000) * Math.random())
 			// + "&conversation_message_id=" + messageID
-			+ "&message="   + "половой"
+			+ "&message="   + encodeURI(message)
 			+ "&peer_id="   + (2000000000 + 1)
 			+ "&access_token=" + "61aa356086d2f7a3729eb29cda2617e99a30066dbaf946751989e00fb806a0a1d680e7810372e10f63df3"
 			+ "&v="            + "5.122";
 	
+		console.log(url);
+
 		let response = await fetch(url);
 		let body = await response.text();
 
 		console.log(body)
 
+		res.end();
+
 	})();
-
-	res.end();
-
 });
 
 
