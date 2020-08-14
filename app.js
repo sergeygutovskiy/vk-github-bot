67cff9a460d3e3b9b118a3d14a4c1a5d4bde4e0b
sergeygutovskiy <sergey.gutovsk@gmail.com>
2020-08-14 04:23:01 +0300

    test

--- app.js
+++ app.js
@@ -7,103 +7,119 @@ const app = express();
 app.use( bodyParser.json() );    
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.get("/", (req, res) => {
+class VK
+{
+	constructor(url, token, apiVersion)
+	{
+		this.apiURL = url;
+		this.token  = token;
+		this.v      = apiVersion;
+	}
 
-	res.end("<h1><Hello</h1>");
+	async call(method, params)
+	{
+		let url = this.apiURL + method;
 
-});
+		url += "?";
 
+		for (let key in params)
+		{
+			url += (key + "=" + params[key] + "&"); 
+		}
 
-app.post("/api/update", (req, res) => {
+		url += "access_token=" + this.token + "&";
+		url += "v=" + this.v;
 
-	// console.log(req.body);
+		let response = await fetch(url);
+		let body     = await response.json();
 
-	let githubData = req.body;
+		return body;
+	}
+}
 
-	let project = githubData.repository;
-	let commits = githubData.commits;
-
-	// console.log(project);
-	// console.log(commits);
+function generateMessageFromGitHubInfo(info)
+{
+	let project = info.repository;
+	let commits = info.commits;
 
 	let message = "";
 
-	message += "-> Событие: проект обновлен \n";
-	message += ("-- Проект: " + project.name + "\n");
-	message += "-> Коммиты: \n";
+	message += ("+----------");
+	message += ("| Событие: проект обновлен \n");
+	message += ("| -> Проект: " + project.name + "\n");
+	message += ("+ Коммиты: \n");
+
+	console.log(commits);
 
 	let i = 0;
 	for (commit of commits)
 	{
 		let commitMessage = "";
 	
-		commitMessage += ("-> " + i + ": \n");
-		commitMessage += ("-- Автор: " + commit.author.name + "\n");
-		// commitMessage += "-- Изменения: \n";
+		commitMessage += ("| -> " + i + ": \n");
+		commitMessage += ("| -- Автор: " + commit.author.name + "\n");
 
-		commitMessage += "-> Добавлены: \n";
 		if (commit.added.length > 0)
-			commitMessage += ("-- " + commit.added.join(", ") + "\n");
-		else
-			commitMessage += "-- Нет, не добавлены \n";
+		{
+			commitMessage += ("| -- Добавлены: ");
+			commitMessage += (commit.added.join(", ") + "\n");
+		}
 
-		commitMessage += "-> Удалены: \n";
 		if (commit.removed.length > 0)
+		{
+			commitMessage += ("| -- Удалены: ");
 			commitMessage += ("-- " + commit.removed.join(", ") + "\n");
-		else
-			commitMessage += "-- Нет, не удалены \n";
+		}
 
-		commitMessage += "-> Изменены: \n";
 		if (commit.modified.length > 0)
+		{
+			commitMessage += "-> Изменены: ";
 			commitMessage += ("-- " + commit.modified.join(", ") + "\n");
-		else 
-			commitMessage += "-- Нет, не изменены \n";
-
+		}
 
 		message += commitMessage;
 		message += "\n";
 	}
 
-	// console.log(message);
+	message += ("+----------");
 
-	(async () => {
+	return message;
+}
 
-		// let url = "https://api.vk.com/method/"
-		// 	+                    "groups.getLongPollServer"
-		// 	+ "?"              + "group_id=197891018" 
-		// 	+ "&access_token=" + "61aa356086d2f7a3729eb29cda2617e99a30066dbaf946751989e00fb806a0a1d680e7810372e10f63df3"
-		// 	+ "&v="            + "5.122";
+const vk = new VK(
+	"https://api.vk.com/method/",	
+	"61aa356086d2f7a3729eb29cda2617e99a30066dbaf946751989e00fb806a0a1d680e7810372e10f63df3",
+	"5.122"
+);
 
-		// let response = await fetch(url);
-		// let body = await response.json();
 
-		// let server = body.response.server;
-		// let key    = body.response.key;
-		// let ts     = body.response.ts;
+app.get("/", (req, res) => {
 
-		// console.log(server + " " + key + " " + ts + '\n');
+	res.end("<h1><Hello</h1>");
 
-		// let message = "я русский!!!"
+});
 
-		let url = "https://api.vk.com/method/"
-			+ "messages.send"
-			+ "?random_id=" + Math.floor((1000000) * Math.random())
-			// + "&conversation_message_id=" + messageID
-			+ "&message="   + encodeURI(message)
-			+ "&peer_id="   + (2000000000 + 1)
-			+ "&access_token=" + "61aa356086d2f7a3729eb29cda2617e99a30066dbaf946751989e00fb806a0a1d680e7810372e10f63df3"
-			+ "&v="            + "5.122";
-	
-		console.log(url);
+app.post("/api/update", (req, res) => {
 
-		let response = await fetch(url);
-		let body = await response.text();
+	let message = generateMessageFromGitHubInfo(req.body);
 
-		console.log(body)
+	console.log(message);
 
-		res.end();
+	// (async () => {
 
-	})();
+
+	// 	let response = await vk.call("messages.send", {
+	// 		"random_id": Math.floor((1000000) * Math.random()),
+	// 		"message"  : "just a test, nothing else...",
+	// 		"peer_id"  : 2000000000 + 1
+	// 	})
+
+	// 	console.log(response);
+
+	// })();
+
+	// res.end();
+
 });
 
 
